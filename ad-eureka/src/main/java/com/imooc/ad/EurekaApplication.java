a7de28c30ded79252a870aa7d60aa5d7de708a8c
tangchungjiao <2074623967@qq.com>
2023-05-05 15:59:29 +0800

    Eureka-Server单节点和多节点配置

---
+++ ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author tangcj
+ * @date 2023/05/05 15:33
+ * 单节点eureka-sever配置
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
