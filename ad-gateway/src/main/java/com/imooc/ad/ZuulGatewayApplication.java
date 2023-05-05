74e3d698a329042f381364293945ec4500fd2eca
tangchungjiao <2074623967@qq.com>
2023-05-05 16:27:52 +0800

    网关服务搭建

---
+++ ad-gateway/src/main/java/com/imooc/ad/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author tangcj
+ * @date 2023/05/05 16:15
+ **/
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+}
