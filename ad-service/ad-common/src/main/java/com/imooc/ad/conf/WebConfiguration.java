a67105cc8b01158053c63fa31b41eff22b91eed5
tangchungjiao <2074623967@qq.com>
2023-05-06 10:45:27 +0800

    广告投放系统子模块搭建

--- ad-service/ad-common/src/main/java/com/imooc/ad/conf/WebConfiguration.java
+++ ad-service/ad-common/src/main/java/com/imooc/ad/conf/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.imooc.ad.conf;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +11,7 @@ import java.util.List;
  * @author tangcj
  * @date 2023/05/05 16:51
  **/
+@Configuration
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
---
+++ ad-service/ad-sponsor/src/main/java/com/imooc/ad/SponsorApplication.java
@@ -0,0 +1,22 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author tangcj
+ * @date 2023/05/06 10:23
+ **/
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class SponsorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SponsorApplication.class, args);
+    }
+}
