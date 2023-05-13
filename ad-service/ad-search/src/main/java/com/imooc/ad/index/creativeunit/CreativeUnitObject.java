af881bef8b16a34e04b7eea6ac2a1bb9c01aac9a
tangchungjiao <2074623967@qq.com>
2023-05-13 17:36:56 +0800

    创意与推广单元索引对象定义和实现

---
+++ ad-service/ad-search/src/main/java/com/imooc/ad/index/creativeunit/CreativeUnitObject.java
@@ -0,0 +1,20 @@
+package com.imooc.ad.index.creativeunit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author tangcj
+ * @date 2023/05/13 17:28
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeUnitObject{
+
+    private Long adId;
+
+    private Long unitId;
+
+}
