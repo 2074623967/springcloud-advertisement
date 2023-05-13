1e6c43572eb8ede72a269d312898408fcca7e3bf
tangchungjiao <2074623967@qq.com>
2023-05-13 22:17:22 +0800

    全量索引的加载

---
+++ ad-service/ad-search/src/main/java/com/imooc/ad/mysql/constant/OpType.java
@@ -0,0 +1,28 @@
+package com.imooc.ad.mysql.constant;
+
+import com.github.shyiko.mysql.binlog.event.EventType;
+
+/**
+ * @author tangcj
+ * @date 2023/05/13 21:11
+ **/
+public enum OpType {
+
+    ADD,
+    UPDATE,
+    DELETE,
+    OTHER;
+
+    public static OpType to(EventType eventType) {
+        switch (eventType) {
+            case EXT_WRITE_ROWS:
+                return ADD;
+            case EXT_UPDATE_ROWS:
+                return UPDATE;
+            case EXT_DELETE_ROWS:
+                return DELETE;
+            default:
+                return OTHER;
+        }
+    }
+}
---
+++ ad-service/ad-search/src/main/java/com/imooc/ad/mysql/tesy1.java
@@ -0,0 +1,8 @@
+package com.imooc.ad.mysql;
+
+/**
+ * @author tangcj
+ * @date 2023/05/13 21:10
+ **/
+public class tesy1 {
+}
