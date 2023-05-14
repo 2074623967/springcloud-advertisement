b0ae92d0a8d1ece63356188ad796dc0df9f7c2cd
tangchungjiao <2074623967@qq.com>
2023-05-14 14:03:28 +0800

    BinLog监听与解析的实现

---
+++ ad-service/ad-search/src/main/java/com/imooc/ad/listener/Ilistener.java
@@ -0,0 +1,23 @@
+package com.imooc.ad.listener;
+
+import com.imooc.ad.mysql.dto.BinlogRowData;
+
+/**
+ * @author tangcj
+ * @date 2023/05/14 12:58
+ * 监听器
+ **/
+public interface Ilistener {
+
+    /**
+     * 对应不同表定义不同的数据更新方法,即注册不同的监听器.
+     **/
+    void register();
+
+    /**
+     * 事件监听
+     * @param eventData
+     * @return void
+     **/
+    void onEvent(BinlogRowData eventData);
+}
---
+++ ad-service/ad-search/src/main/java/com/imooc/ad/mysql/dto/BinlogRowData.java
@@ -0,0 +1,24 @@
+package com.imooc.ad.mysql.dto;
+
+import com.github.shyiko.mysql.binlog.event.EventType;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author tangcj
+ * @date 2023/05/14 12:55
+ * 日志文件相应的Java对象
+ **/
+@Data
+public class BinlogRowData {
+
+    private TableTemplate table;
+
+    private EventType eventType;
+
+    private List<Map<String, String>> after;
+
+    private List<Map<String, String>> before;
+}
