4f7d28ffe28bd8549ed812252c9166020c4b2553
tangchungjiao <2074623967@qq.com>
2023-05-17 21:16:17 +0800

    获取创意实现并填充检索服务响应对象

--- ad-service/ad-search/src/main/java/com/imooc/ad/index/adunit/AdUnitConstants.java
+++ ad-service/ad-search/src/main/java/com/imooc/ad/index/adunit/AdUnitConstants.java
@@ -8,29 +8,19 @@ public class AdUnitConstants {
 
     public static class POSITION_TYPE {
 
-        /**
-         * 开屏广告位
-         */
+        //开屏广告位
         public static final int KAIPING = 1;
 
-        /**
-         * 贴片广告
-         */
+        //贴片广告
         public static final int TIEPIAN = 2;
 
-        /**
-         * 视频播放中显示的广告
-         */
+        //视频播放中显示的广告
         public static final int TIEPIAN_MIDDLE = 4;
 
-        /**
-         * 视频播放暂停时广告
-         */
+        //视频播放暂停时广告
         public static final int TIEPIAN_PAUSE = 8;
 
-        /**
-         * 视频播放后的广告位
-         */
+        //视频播放后的广告位a
         public static final int TIEPIAN_POST = 16;
     }
 }
